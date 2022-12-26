1c14c69b59474e569237a2936f29ee9a09ef2a10
par jin sung <parkgutime@gmail.com>
2022-12-26 12:00:13 +0900

    몽고 DB 서버만듦, JWT 토큰 설정

--- shop-server/db.js
+++ shop-server/db.js
@@ -5,4 +5,12 @@ module.exports = () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
+
+  try {
+    mongoose.connect(process.env.DB, connectionParams);
+    console.log("Connected to database successfully!");
+  } catch (error) {
+    console.log(error);
+    console.log("could not connection");
+  }
 };
--- shop-server/index.js
+++ shop-server/index.js
@@ -2,9 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const connection = require("./db");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+// 데이터베이스 커넥션
+connection();
 
 // 미들웨어
 app.use(express.json());
+app.use(cors());
+
+//routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", userRoutes);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
