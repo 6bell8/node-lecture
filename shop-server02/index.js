c73664c2db3e104443c21cb25462ed4558fc2f80
par jin sung <parkgutime@gmail.com>
2023-01-02 10:27:41 +0900

    로그인 서버 완료

--- shop-server02/index.js
+++ shop-server02/index.js
@@ -18,5 +18,17 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
+// 수정 부분
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// app.get("/", (req, res) => {
+//   res.send("APP is running.");
+// });
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
